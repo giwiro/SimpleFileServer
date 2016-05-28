54fa395c237a817d1ae5f3d98721dec63fc85436
Gi Wah <giwirodavalos@gmail.com>
2016-05-28 17:18:12 -0500

    fix de variable del puerto

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var package = require('./package');
 
+var port = package.port || 80;
+
 app.use(express.static(__dirname + '/public'));
 
-app.listen(package.port || 80);
+app.listen(port);
+console.log("listening on port: " + port);
