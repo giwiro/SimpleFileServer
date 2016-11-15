8e2702b50e8dd35e0b1b76bbb54b9a37578fd031
Gi Wah <giwah_loo@yahoo.es>
2016-11-15 16:34:33 -0500

    get public ip

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
-var package = require('./package');
 
-var port = package.port || 80;
+var port = process.env.PORT || 80;
+
+app.get('/public/banner.jpg', function (req, res) {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log('ip', ip);
+  return res.status(200).send(' ');
+})
 
 app.use(express.static(__dirname + '/public'));
+
+
 
 app.listen(port);
 console.log("listening on port: " + port);
